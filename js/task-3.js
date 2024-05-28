81f1b189f0e0b8dfc67b6a80a8d02996fe3cc9a1
Nadija Smerechuk <nadija1smerechuk@gmail.com>
2024-05-28 21:51:41 +0300

    rredact task 6

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,25 @@
 class StringBuilder {
-    
+    #value;
     constructor(initialValue){
-        this.value = initialValue;
+        this.#value = initialValue;
     }
     getValue() {
-        return this.value;
+        return this.#value;
     }
 
     padEnd(str) {
-        this.value += str;
+        this.#value += str;
     }
 
     padStart(str) {
-        this.value = str + this.value;
+        this.#value = str + this.#value;
     }
 
     padBoth(str) {
-        this.value = str + this.value + str;
+        this.#value = str + this.#value + str;
     }
 
 }
-
 
 
 const builder = new StringBuilder(".");
@@ -30,4 +29,4 @@ console.log(builder.getValue()); // "^."
 builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
 builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^
+console.log(builder.getValue()); // "=^.^="
